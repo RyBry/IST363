fcb3bd5f3fc26f55bfffbbff8d8f5d77d9ff2fa9
RyBry <rion257@outlook.com>
2024-02-08 18:22:05 -0500

    attendance 2/8/24

--- js/main.js
+++ js/main.js
@@ -46,37 +46,37 @@ const rooms = [
   },
 ];
 
-rooms.forEach((room) => {
-  const roomArticle = document.createElement('article');
-  roomArticle.classList.add('room');
-  /* Add the "room" css class to room article */
+function renderProperties(properties) {
+  properties.forEach((room) => {
+    const roomArticle = document.createElement('article');
+    roomArticle.classList.add('room');
+    /* Add the "room" css class to room article */
 
-  const roomNameElement = document.createElement('h3');
-  roomNameElement.classList.add('room--name');
-  roomNameElement.textContent = room.name;
+    const roomNameElement = document.createElement('h3');
+    roomNameElement.classList.add('room--name');
+    roomNameElement.textContent = room.name;
 
-  const roomDescriptionElement = document.createElement('p');
-  roomDescriptionElement.classList.add('room--description');
-  roomDescriptionElement.textContent = room.description;
+    const roomDescriptionElement = document.createElement('p');
+    roomDescriptionElement.classList.add('room--description');
+    roomDescriptionElement.textContent = room.description;
 
-  const roomPriceElement = document.createElement('p');
-  roomPriceElement.textContent = `Price: ${room.price}`;
+    const roomPriceElement = document.createElement('p');
+    roomPriceElement.textContent = `Price: ${room.price}`;
 
-  const roomGuestsElement = document.createElement('p');
-  roomGuestsElement.textContent = `Guests: ${room.guests}`;
+    const roomGuestsElement = document.createElement('p');
+    roomGuestsElement.textContent = `Guests: ${room.guests}`;
 
-  roomArticle.appendChild(roomNameElement);
-  roomArticle.appendChild(roomDescriptionElement);
-  roomArticle.appendChild(roomPriceElement);
-  roomArticle.appendChild(roomGuestsElement);
+    roomArticle.appendChild(roomNameElement);
+    roomArticle.appendChild(roomDescriptionElement);
+    roomArticle.appendChild(roomPriceElement);
+    roomArticle.appendChild(roomGuestsElement);
 
-  document.body.appendChild(roomArticle);
-});
+    document.body.appendChild(roomArticle);
+  });
+} // end of renderProperties
 
-// const room = {
-//   name: 'Luxury King Room',
-//   price: 300,
-//   guests: 2,
-//   description:
-//     'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
-// };
+fetch('/js/properties.json')
+  .then((response) => response.json())
+  .then((data) => {
+    renderProperties(data);
+  });
