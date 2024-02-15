cd55f8e5e4a9c0ce16420040f123c329801b0756
RyBry <rion257@outlook.com>
2024-02-15 18:22:30 -0500

    attendance 02/15/24, added sorting

--- js/main.js
+++ js/main.js
@@ -81,8 +81,66 @@ function renderProperties(properties) {
 //     renderProperties(data);
 //   });
 
+const displayCategory = (category, properties) => {
+  // console.log(category);
+  const sectionElement = document.createElement('section');
+  sectionElement.classList.add('category');
+
+  const sectionTitle = document.createElement('h2');
+  sectionTitle.textContent = category.label.plural;
+
+  sectionElement.appendChild(sectionTitle);
+
+  //1. Filter properties
+  // Loop thorught properties, place the element into "property"
+  console.log(category.label.singular);
+  // .filter() builds an array into const filteredProperties with every item which returned "true." Yes this is horrible.
+  const filteredProperties = properties.filter((property) => {
+    return category.label.singular === property.type;
+    // return the elements into the list where the singular name matches the requested property type.
+  });
+
+  // sort performs "quickSort" based on the provided parameters.
+  // -1 moves a before b. 1 moves a after b. 0 says there the same order.
+  filteredProperties.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+
+  // console.log({ filteredProperties });
+  filteredProperties.forEach((property) => {
+    const articleElement = document.createElement('article');
+    articleElement.classList.add('property');
+
+    //const propertyTitle = document.createElement('h3');
+    //propertyTitle.classList.add('property--title');
+
+    // Backticks allow writing HTML
+    let propertyHtml = `
+      <h3 class="property--title">${property.name}</h3>
+      <p class="property--description">${property.description}</p>
+      <p class="property--price">${property.price}</p>
+    `;
+
+    articleElement.innerHTML = propertyHtml;
+    sectionElement.appendChild(articleElement);
+  }); // end of forEach
+
+  // TODO: COPY PASTE THESE
+
+  //2. Loop and append properties
+
+  document.body.appendChild(sectionElement);
+}; // end of displayCategory
+
 Promise.all([
-  // fetch 1
+  // fetch 1. When response is fetched, it is just interpreted as a string,
+  // even though the file ends in .JSON. We must tell the system it is a JSON.
   fetch('js/properties.json').then((response) => response.json()),
   // fetch 2
   fetch('js/categories.json').then((response) => response.json()),
@@ -97,15 +155,3 @@ Promise.all([
   .catch((error) => {
     console.error('There was a problem fetching the data:', error);
   });
-
-const displayCategory = (category, properties) => {
-  // console.log(category);
-  const sectionElement = document.createElement('section');
-
-  const sectionTitle = document.createElement('h2');
-  sectionTitle.textContent = category.label.plural;
-
-  sectionElement.appendChild(sectionTitle);
-
-  document.body.appendChild(sectionElement);
-}; // end of displayCategory
