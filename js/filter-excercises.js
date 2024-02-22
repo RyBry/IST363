60923d0b31374c80099da846624c57cbf235aca4
RyBry <rion257@outlook.com>
2024-02-22 17:42:45 -0500

    homework: schedule json

--- js/filter-excercises.js
+++ js/filter-excercises.js
@@ -9,7 +9,7 @@ const filteredNumbers = numbers.filter((number) => {
   return number > 5 && number < 10;
 }); // end filteredNumbers
 
-console.log({ filteredNumbers });
+//console.log({ filteredNumbers });
 
 // Example 2
 const people = [
@@ -25,7 +25,7 @@ const filteredPeople = people.filter((person) => {
   return person.age < 30;
 }); // end of filter
 
-console.log(filteredPeople);
+//console.log(filteredPeople);
 
 // Exercise #3
 // 1. Create an array of objects containing Ford and Chevrolet cars
@@ -42,35 +42,25 @@ const filteredCars = cars.filter((car) => {
   return car.make == 'Ford';
 }); // end of filter
 
-console.log(filteredCars);
+//console.log(filteredCars);
 
-// Exercise #4
-// 1. Fetch a JSON array of objects containing the SU men's basketball schedule (please download the entire schedule.json file from Blackboard/Content below tonight's agenda)
+const filterGames = (gameData, monthIndex) => {
+  const filteredGames = gameData.filter((game) => {
+    const dateObject = new Date(game.date);
+    return dateObject.getMonth() === monthIndex;
+  });
 
-// BELOW IS JUST AN EXAMPLE---YOU WILL HAVE TO IMPORT!!!
-const schedule = [
-  {
-    date: '2023-10-27',
-    opponent: 'Deamon',
-    location: 'JMA Wireless Dome',
-    exhibition: true,
-  },
-  {
-    date: '2023-11-01',
-    opponent: 'College of St. Rose',
-    location: 'JMA Wireless Dome',
-    exhibition: true,
-  },
-  // etc.
-];
+  console.log({ filteredGames });
+};
 
-// 2. Use the filter method to only include the games in February
-// Big question: How do you evaluate the date property in the filter method to only include games in February? Convert the date string to a Date object and evaluate the month property.
-// 3. Print the result to the console
-
-const februaryGames = schedule.filter((game) => {
-  // write your code here
-  const dateObj = new Date(game.date); // convert the date string to a Date object
-  return dateObj.getMonth() === 1; // February is the second month, so the index is 1, January is 0, March is 2, April is 3, etc.
-});
-console.log(februaryGames);
+// Example 4
+// fetch pulls in data, then converts to json, next then processes, catch is there for errors
+fetch('js/schedule.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    filterGames(data, 0);
+    //console.log({ data });
+  })
+  .catch(); // promise
